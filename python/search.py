1b65fa5bc0809d297985a3cd556668b4dbc1fc10
Tuan Tran <tuan@Anhs-MacBook-Pro.local>
2017-03-28 13:52:09 +0200

    debug

--- python/search.py
+++ python/search.py
@@ -155,7 +155,7 @@ def memefficientrerankedsearch(es, term, k, func):
     try:
         while cnt < k and sys.getsizeof(cache) < MEMSIZE:
             hit = next(res)
-            print(hit)
+            print(str(hit).decode('utf-8'))
             doc = hit['hits']['hits']
             doc['_source']['text'] = ''  # enable GC
 
