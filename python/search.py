c79f7addcce6a78a9c5846c8ab0ce332e6376fa2
Tuan Tran <tuan@Anhs-MacBook-Pro.local>
2017-03-28 13:43:21 +0200

    sync

--- python/search.py
+++ python/search.py
@@ -127,7 +127,7 @@ def memefficientrerankedsearch(es, term, k, func):
     max_batch = min(k,100) # The maximal number of results returned in one batch. This number should be
                            # tuned based on the average size of document length, the number of shards,
                            # and the main memory of the client.
-    res = es.search(es,
+    res = es.search(
         index='wiki',
         doc_type='page',
         scroll='1m', # Keep the connection alive for max 1 min
